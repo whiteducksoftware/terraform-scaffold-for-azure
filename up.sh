7c7317128fb1bab001c502e06139355fc8688258
Nico Meisenzahl <nico@meisenzahl.org>
2021-06-28 10:53:14 +0200

    updates api permissions

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # used to bootstrap infrastructure required my Terraform
+set -e
 
 # sources secrets; dev only
 if [ -f "./.creds.sh" ]; then
@@ -49,9 +50,13 @@ az ad app permission add \
     0e263e50-5827-48a4-b97c-d940288653c7=Scope \
     c5366453-9fb0-48a5-a156-24f0c49a4b84=Scope \
     4e46008b-f24c-477d-8fff-7bb4ec7aafe0=Scope \
+    e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope \
+    bdfbf15f-ee85-4955-8675-146e8e5296b5=Scope \
     bf7b1a76-6e77-406b-b258-bf5c7720e98f=Role \
     19dbc75e-c2e2-444c-a770-ec69d8559fc7=Role \
-    62a82d76-70ea-41e2-9197-370581804d09=Role
+    62a82d76-70ea-41e2-9197-370581804d09=Role \
+    18a4783c-866b-4cc7-a460-3d5e5662c884=Role \
+    1bfefb4e-e0b5-418b-a88f-73c46d2cc8e9=Role
 
 if test $? -ne 0
 then
