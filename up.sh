0784d07e5e27d5f9eaa224c1a0e7b80d8d4c584a
Dario Brozovic <dario.brozovic@whiteduck.de>
2023-09-19 09:34:17 +0200

    removing unneeded api permission from up.sh

--- up.sh
+++ up.sh
@@ -71,11 +71,6 @@ az ad app permission add \
     --id $spId \
     --api 00000003-0000-0000-c000-000000000000 \
     --api-permissions \
-    0e263e50-5827-48a4-b97c-d940288653c7=Scope \
-    c5366453-9fb0-48a5-a156-24f0c49a4b84=Scope \
-    4e46008b-f24c-477d-8fff-7bb4ec7aafe0=Scope \
-    e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope \
-    bdfbf15f-ee85-4955-8675-146e8e5296b5=Scope \
     bf7b1a76-6e77-406b-b258-bf5c7720e98f=Role \
     19dbc75e-c2e2-444c-a770-ec69d8559fc7=Role \
     62a82d76-70ea-41e2-9197-370581804d09=Role \
